910695027d9f13f33655729ad42237bc39ae281c
Maheesha <maheeshasithum@gmail.com>
2022-09-12 10:50:37 +0530

    Crypt and Unvanish

--- src/main/java/com/github/maheevil/glacier/EventListener.java
+++ src/main/java/com/github/maheevil/glacier/EventListener.java
@@ -2,19 +2,41 @@ package com.github.maheevil.glacier;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 
 @SuppressWarnings("deprecation")
 public class EventListener implements Listener {
     @EventHandler
     public void preCommand(PlayerCommandPreprocessEvent commandEvent){
-        if (commandEvent.getMessage().contains("vanish")) {
+        if(commandEvent.getMessage().contains("vanish") && !commandEvent.getMessage().contains("unvanish")) {
             GlacierTweaksPlugin.server.sendMessage(Component.text(
                     commandEvent.getPlayer().getDisplayName() + " left the game"
             ).color(NamedTextColor.YELLOW));
+        }else if(commandEvent.getMessage().contains("unvanish")){
+            commandEvent.setMessage("/vanish");
+            GlacierTweaksPlugin.server.sendMessage(Component.text(
+                    commandEvent.getPlayer().getDisplayName() + " joined the game"
+            ).color(NamedTextColor.YELLOW));
         }
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent respawnEvent){
+        if(respawnEvent.isBedSpawn() || respawnEvent.isAnchorSpawn()){
+            return;
+        }
+        respawnEvent.setRespawnLocation(
+                new Location(
+                        respawnEvent.getRespawnLocation().getWorld(),
+                        GlacierTweaksPlugin.config.getInt("cryptx"),
+                        GlacierTweaksPlugin.config.getInt("crypty"),
+                        GlacierTweaksPlugin.config.getInt("cryptz")
+                )
+        );
     }
 }
--- src/main/java/com/github/maheevil/glacier/GlacierTweaksPlugin.java
+++ src/main/java/com/github/maheevil/glacier/GlacierTweaksPlugin.java
@@ -1,13 +1,34 @@
 package com.github.maheevil.glacier;
 
 import org.bukkit.Server;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GlacierTweaksPlugin extends JavaPlugin {
     public static Server server;
+    public static GlacierTweaksPlugin glacierTweaksPlugin;
+    public static Configuration config;
     @Override
     public void onEnable(){
+        this.reloadConfig();
+        glacierTweaksPlugin = this;
+        config = this.getConfig();
         this.getServer().getPluginManager().registerEvents(new EventListener(),this);
+        this.getCommand("setCrypt").setExecutor(
+                (sender, command, label, args) -> {
+                    try{
+                        Configuration config = GlacierTweaksPlugin.config;
+                        config.set("cryptx",Integer.parseInt(args[0]));
+                        config.set("crypty",Integer.parseInt(args[1]));
+                        config.set("cryptz",Integer.parseInt(args[2]));
+                        glacierTweaksPlugin.saveConfig();
+                        return true;
+                    }catch(NumberFormatException e){
+                        e.printStackTrace();
+                        return false;
+                    }
+                }
+        );
         server = this.getServer();
     }
 
